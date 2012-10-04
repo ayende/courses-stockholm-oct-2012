a0c52161f80fe660a11b028569b5fc193f15eb2b
Ayende Rahien <ayende@ayende.com>
2012-10-04 11:09:18 +0200

    Adding ravendb controller

---
+++ Stockholm.Syndrom/Infrastructure/RavenController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Web.Mvc;
+using Raven.Client;
+using Raven.Client.Document;
+using Raven.Client.Indexes;
+
+namespace Stockholm.Syndrom.Infrastructure
+{
+	public abstract class RavenController : Controller
+	{
+		protected override JsonResult Json(object data, string contentType, System.Text.Encoding contentEncoding, JsonRequestBehavior behavior)
+		{
+			return base.Json(data, contentType, contentEncoding, JsonRequestBehavior.AllowGet);
+		}
+
+		private static readonly Lazy<IDocumentStore> documentStore =
+			new Lazy<IDocumentStore>(() =>
+				{
+					var store = new DocumentStore
+						{
+							Url = "http://localhost:8080",
+							DefaultDatabase = "Stockholm",
+						}
+						.Initialize();
+
+					//IndexCreation.CreateIndexes(typeof(RavenController).Assembly, store);
+					return store;
+				});
+
+		public static IDocumentStore DocumentStore
+		{
+			get
+			{
+				return documentStore.Value;
+			}
+		}
+
+		public new IDocumentSession Session { get; set; }
+
+		protected override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			Session = DocumentStore.OpenSession();
+		}
+
+		protected override void OnActionExecuted(ActionExecutedContext filterContext)
+		{
+			using (Session)
+			{
+				if (Session == null)
+					return;
+				if (filterContext.Exception != null)
+					return;
+				Session.SaveChanges();
+			}
+		}
+	}
+}
