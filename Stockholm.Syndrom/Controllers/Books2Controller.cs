9a6628a4a13589960a47fc83af9c44ce6f8a35eb
Ayende Rahien <ayende@ayende.com>
2012-10-05 10:30:08 +0200

    Lazy calls

---
+++ Stockholm.Syndrom/Controllers/Books2Controller.cs
@@ -0,0 +1,31 @@
+﻿using Stockholm.Syndrom.Indexes;
+using Stockholm.Syndrom.Infrastructure;
+using Stockholm.Syndrom.Models;
+using Raven.Client;
+using System.Linq;
+using Raven.Client.Linq;
+
+namespace Stockholm.Syndrom.Controllers
+{
+	public class Books2Controller : RavenController
+	{
+		public object Search(string q)
+		{
+			var results = Session.Query<Book, Books_Search>()
+				.Search(x => x.Name, q)
+				.Lazily();
+
+			var authors = Session.Query<Author, Authors_Search>()
+				.Search(x => x.Name, q)
+				.Lazily();
+
+			var authorIds = authors.Value.Select(y => y.Id).ToArray();
+			var authorBooks = Session.Query<Books_Search.AuthorSearchResult, Books_Search>()
+				.Where(x => x.Authors.In(authorIds))
+				.As<Book>()
+				.ToList();
+
+			return Json(results.Value.Concat(authorBooks).ToList());
+		}
+	}
+}
--- Stockholm.Syndrom/Indexes/Books_Search.cs
+++ Stockholm.Syndrom/Indexes/Books_Search.cs
@@ -7,15 +7,34 @@ namespace Stockholm.Syndrom.Indexes
 {
 	public class Books_Search : AbstractIndexCreationTask<Book>
 	{
+		public class AuthorSearchResult
+		{
+			public string Authors { get; set; }
+		}
 		public Books_Search()
 		{
 			Map = books =>
-			      from book in books
-			      select new
-				      {
-					      book.Name
-				      };
-			Index(x=>x.Name, FieldIndexing.Analyzed);
+				  from book in books
+				  select new
+					  {
+						  book.Name,
+						  book.Authors
+					  };
+			Index(x => x.Name, FieldIndexing.Analyzed);
+		}
+	}
+
+	public class Authors_Search : AbstractIndexCreationTask<Author>
+	{
+		public Authors_Search()
+		{
+			Map = authors =>
+				  from author in authors
+				  select new
+					  {
+						  author.Name,
+					  };
+			Index(x => x.Name, FieldIndexing.Analyzed);
 		}
 	}
 }
